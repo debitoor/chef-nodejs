c3d2e8ed3c246990ca686a9a634d31398d124158
Brian Hansen <affelaffen@gmail.com>
2015-06-24 12:16:17 +0200

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -25,7 +25,7 @@ default['nodejs']['npm'] = '1.2.14'
 default['nodejs']['checksum'] =
   'ef5e4ea6f2689ed7f781355012b942a2347e0299da0804a58de8e6281c4b1daa'
 default['nodejs']['checksum_linux_x64'] =
-  '305bf2983c65edea6dd2c9f3669b956251af03523d31cf0a0471504fd5920aac'
+  '5f41f4a90861bddaea92addc5dfba5357de40962031c2281b1683277a0f75932'
 default['nodejs']['checksum_linux_x86'] =
   '8fa2d952556c8b5aa37c077e2735c972c522510facaa4df76d4244be88f4dc0f'
 
